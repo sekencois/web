b59ab2e05ec2354dfc7e434524edf268d70971b4
sekencois <161124134+sekencois@users.noreply.github.com>
2024-03-05 13:37:19 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,8 +10,9 @@ function validateForm() {
   var password = document.getElementById("password").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
-  if (username === "nila" && password === "24") {
-    if (username === "nir" && password === "27") {
+  if (username === "nila" && password === "24") 
+  (username === "admin" && password === "27"){
+    
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
